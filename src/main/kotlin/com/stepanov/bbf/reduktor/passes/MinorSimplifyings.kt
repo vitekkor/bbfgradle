978b10ad361b0edac4a4679e66ad1d6305851ff5
DaniilStepanov <stepanov0995@mail.ru>
2020-01-14 18:49:49 +0300

    little reduKtor improvements

--- src/main/kotlin/com/stepanov/bbf/reduktor/passes/MinorSimplifyings.kt
+++ src/main/kotlin/com/stepanov/bbf/reduktor/passes/MinorSimplifyings.kt
@@ -3,31 +3,58 @@ package com.stepanov.bbf.reduktor.passes
 import com.stepanov.bbf.reduktor.executor.CompilerTestChecker
 import com.stepanov.bbf.reduktor.util.generateDefValuesAsString
 import com.stepanov.bbf.reduktor.util.getAllPSIChildrenOfType
-import org.jetbrains.kotlin.psi.KtFile
-import org.jetbrains.kotlin.psi.KtNameReferenceExpression
-import org.jetbrains.kotlin.psi.KtPsiFactory
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.getValueParameters
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.calls.callUtil.getType
 
 //
-class MinorSimplifyings(private val file: KtFile, private val checker: CompilerTestChecker,
-                        private val ctx: BindingContext) {
+class MinorSimplifyings(
+    private val file: KtFile, private val checker: CompilerTestChecker,
+    private val ctx: BindingContext
+) {
+
     fun transform() {
         //Replace variable on constant if we can get type
         try {
-            file.getAllPSIChildrenOfType<KtNameReferenceExpression>().forEach {
-                it.getType(ctx)?.let { type ->
-                    val exp = KtPsiFactory(file.project).createExpression(
-                        generateDefValuesAsString(
-                            type.toString()
-                        )
-                    )
-                    checker.replaceNodeIfPossible(file, it, exp)
+            file.getAllPSIChildrenOfType<KtExpression>()
+                    //TODO DO SMTH WITH THAT
+                .filter { it !is KtConstantExpression &&
+                        (it is KtReferenceExpression || it is KtOperationExpression || it is KtCallExpression) }
+                .forEach {
+                    it.getType(ctx)?.let { type ->
+                        val defValue =
+                            customStructures.find { it.second == "$type" }?.let { getDefValueForCustomClass(it.first) }
+                                ?: generateDefValuesAsString("$type")
+                        val exp = KtPsiFactory(file.project).createExpression(defValue)
+                        checker.replaceNodeIfPossible(file, it, exp)
+                    }
                 }
-            }
         } catch (e: Exception) {
-           return
+            return
         }
     }
+
+    private fun getDefValueForCustomClass(classOrObject: KtClassOrObject): String? {
+        if (classOrObject is KtObjectDeclaration) return classOrObject.name
+        if (classOrObject is KtClass) {
+            val res = StringBuilder()
+            val constructor = classOrObject.primaryConstructor ?: return "${classOrObject.name}()"
+            res.append("${classOrObject.name}(")
+            for (par in constructor.valueParameters) {
+                val type = par.typeReference ?: return null
+                val defValue =
+                    customStructures.find { it.second == type.text }?.let { getDefValueForCustomClass(it.first) }
+                        ?: generateDefValuesAsString(type.text)
+                res.append("${defValue},")
+            }
+            res.deleteCharAt(res.length - 1)
+            res.append(")")
+            return res.toString()
+        }
+        return null
+    }
+
+    val customStructures = file.getAllPSIChildrenOfType<KtClassOrObject>().map { it to it.name }
 
 }
--- src/main/kotlin/com/stepanov/bbf/reduktor/passes/SimplifyIf.kt
+++ src/main/kotlin/com/stepanov/bbf/reduktor/passes/SimplifyIf.kt
@@ -11,16 +11,15 @@ import org.jetbrains.kotlin.psi.psiUtil.allChildren
 
 class SimplifyIf(private val file: KtFile, private val checker: CompilerTestChecker) {
 
-    fun transform() {
-        val ifExpressions = file.getAllPSIChildrenOfType<KtIfExpression>()
+    private fun replaceIfOnCondition(ifExpressions: List<KtIfExpression>, isIfExp: Boolean) {
         for (ifExp in ifExpressions) {
-            //Try to replace if on it condition if condition is BinaryExpression
             if (ifExp.condition is KtBinaryExpression) {
                 val cond = ifExp.condition as KtBinaryExpression
                 if (cond.children.size >= 3) {
                     var res: PsiElement? = null
                     if (cond.operationReference.prevSibling != null) {
-                        res = checker.replaceNodeOnItChild(file, ifExp.node, cond.left!!.node)?.psi
+                        res = if (isIfExp) checker.replaceNodeOnItChild(file, ifExp.node, cond.left!!.node)?.psi
+                        else checker.replaceNodeOnItChild(file, cond.node, cond.left!!.node)?.psi
                         if (res == null) continue
                     }
                     val binRes = res as? KtBinaryExpression
@@ -29,30 +28,27 @@ class SimplifyIf(private val file: KtFile, private val checker: CompilerTestChec
                     }
                 }
             }
+        }
+    }
 
-            //Try to replace if condition is BinaryExpression on left or right side
-            if (ifExp.condition is KtBinaryExpression) {
-                val cond = ifExp.condition as KtBinaryExpression
-                if (cond.children.size >= 3) {
-                    var res: PsiElement? = null
-                    if (cond.operationReference.prevSibling != null) {
-                        res = checker.replaceNodeOnItChild(file, cond.node, cond.left!!.node)?.psi
-                        if (res == null) continue
-                    }
-                    val binRes = res as? KtBinaryExpression
-                    if (binRes != null && binRes.operationReference.nextSibling != null) {
-                        if (checker.replaceNodeOnItChild(file, binRes.node, binRes.right!!.node) == null) continue
-                    }
-                }
+    private fun tryToReplaceCondOnConst(ifExpressions: List<KtIfExpression>) {
+        for (ifExp in ifExpressions) {
+            if (ifExp.condition != null) {
+                val trueExp = psiFactory.createExpression("true")
+                checker.replaceNodeIfPossible(file, ifExp.condition!!, trueExp)
             }
+        }
+    }
 
-
-            //Try to replace if on then
+    private fun tryToReplaceCondOnItPart(ifExpressions: List<KtIfExpression>) {
+        for (ifExp in ifExpressions) {
             ifExp.then?.let { then ->
                 val oldIf = ifExp.copy() as KtIfExpression
                 //Remove rbrace and lbrace from then
-                then.allChildren.first?.replaceThis(psiFactory.createWhiteSpace())
-                then.allChildren.last?.replaceThis(psiFactory.createWhiteSpace())
+                if (then is KtBlockExpression) {
+                    then.lBrace?.replaceThis(psiFactory.createWhiteSpace())
+                    then.rBrace?.replaceThis(psiFactory.createWhiteSpace())
+                }
                 //Replace node if possible
                 ifExp.replaceThis(then)
                 if (!checker.checkTest(file.text)) {
@@ -62,7 +58,8 @@ class SimplifyIf(private val file: KtFile, private val checker: CompilerTestChec
                             then.replaceThis(oldIf)
                             return@let
                         }
-                        val asExp = psiFactory.createExpression("${isExp.leftHandSide.text} as ${isExp.typeReference?.text}")
+                        val asExp =
+                            psiFactory.createExpression("${isExp.leftHandSide.text} as ${isExp.typeReference?.text}")
                         then.addBefore(asExp, then.firstChild)
                         if (!checker.checkTest(file.text)) {
                             then.replaceThis(oldIf)
@@ -81,17 +78,103 @@ class SimplifyIf(private val file: KtFile, private val checker: CompilerTestChec
                     }
                 }
             }
-            //Try to replace if condition on constant
-            if (ifExp.condition != null) {
-                val trueExp = psiFactory.createExpression("true")
-                val falseExp = psiFactory.createExpression("false")
-                if (!checker.replaceNodeIfPossible(file, ifExp.condition!!.node, trueExp.node))
-                    checker.replaceNodeIfPossible(file, ifExp.condition!!.node, falseExp.node)
-            }
         }
+    }
+
+    fun transform() {
+        //Try to replace if on it condition if condition is BinaryExpression
+        replaceIfOnCondition(file.getAllPSIChildrenOfType<KtIfExpression>(), true)
+        //Try to replace if condition is BinaryExpression on left or right side
+        replaceIfOnCondition(file.getAllPSIChildrenOfType<KtIfExpression>(), false)
+        //Try to replace if condition on constant
+        tryToReplaceCondOnConst(file.getAllPSIChildrenOfType<KtIfExpression>())
+        //Try to replace if on then
+        tryToReplaceCondOnItPart(file.getAllPSIChildrenOfType<KtIfExpression>())
+
+//        val ifExpressions = file.getAllPSIChildrenOfType<KtIfExpression>()
+//        for (ifExp in ifExpressions) {
+//            //Try to replace if on it condition if condition is BinaryExpression
+//            if (ifExp.condition is KtBinaryExpression) {
+//                val cond = ifExp.condition as KtBinaryExpression
+//                if (cond.children.size >= 3) {
+//                    var res: PsiElement? = null
+//                    if (cond.operationReference.prevSibling != null) {
+//                        res = checker.replaceNodeOnItChild(file, ifExp.node, cond.left!!.node)?.psi
+//                        if (res == null) continue
+//                    }
+//                    val binRes = res as? KtBinaryExpression
+//                    if (binRes != null && binRes.operationReference.nextSibling != null) {
+//                        if (checker.replaceNodeOnItChild(file, binRes.node, binRes.right!!.node) == null) continue
+//                    }
+//                }
+//            }
+
+//            //Try to replace if condition is BinaryExpression on left or right side
+//            if (ifExp.condition is KtBinaryExpression) {
+//                val cond = ifExp.condition as KtBinaryExpression
+//                if (cond.children.size >= 3) {
+//                    var res: PsiElement? = null
+//                    if (cond.operationReference.prevSibling != null) {
+//                        res = checker.replaceNodeOnItChild(file, cond.node, cond.left!!.node)?.psi
+//                        if (res == null) continue
+//                    }
+//                    val binRes = res as? KtBinaryExpression
+//                    if (binRes != null && binRes.operationReference.nextSibling != null) {
+//                        if (checker.replaceNodeOnItChild(file, binRes.node, binRes.right!!.node) == null) continue
+//                    }
+//                }
+//            }
+
+        //Try to replace if condition on constant
+//            if (ifExp.condition != null && ifExp.then != null) {
+//                val trueExp = psiFactory.createExpression("true")
+//                val oldIfCopy = psiFactory.createIf(ifExp.condition!!, ifExp.then!!, ifExp.`else`)
+//                val newIfExp = psiFactory.createIf(trueExp, ifExp.then!!, ifExp.`else`)
+//                ifExp.replaceThis(newIfExp)
+//                if (!checker.checkTest(file.text)) {
+//                    newIfExp.replaceThis(oldIfCopy)
+//                }
+//            }
+
+        //Try to replace if on then
+//            ifExp.then?.let { then ->
+//                val oldIf = ifExp.copy() as KtIfExpression
+//                //Remove rbrace and lbrace from then
+//                if (then is KtBlockExpression) {
+//                    then.lBrace?.replaceThis(psiFactory.createWhiteSpace())
+//                    then.rBrace?.replaceThis(psiFactory.createWhiteSpace())
+//                }
+//                //Replace node if possible
+//                ifExp.replaceThis(then)
+//                if (!checker.checkTest(file.text)) {
+//                    if (ifExp.condition is KtIsExpression) {
+//                        val isExp = ifExp.condition as KtIsExpression
+//                        if (isExp.isNegated) {
+//                            then.replaceThis(oldIf)
+//                            return@let
+//                        }
+//                        val asExp =
+//                            psiFactory.createExpression("${isExp.leftHandSide.text} as ${isExp.typeReference?.text}")
+//                        then.addBefore(asExp, then.firstChild)
+//                        if (!checker.checkTest(file.text)) {
+//                            then.replaceThis(oldIf)
+//                        }
+//                    } else {
+//                        if (oldIf.`else` == null) {
+//                            then.replaceThis(oldIf)
+//                        } else {
+//                            val copy = oldIf.copy()
+//                            val el = oldIf.`else`!!
+//                            then.replaceThis(el)
+//                            if (!checker.checkTest(file.text)) {
+//                                el.replaceThis(copy)
+//                            }
+//                        }
+//                    }
+//                }
+//            }
     }
 
     private val psiFactory = KtPsiFactory(file.project)
     private val log = Logger.getLogger("transformationManagerLog")
-
 }
