80637cfc91e91f5a389bbe64d51ab4d16f412bda
DaniilStepanov <stepanov0995@mail.ru>
2020-01-29 19:13:57 +0300

    New transformation for reduktor

---
+++ src/main/kotlin/com/stepanov/bbf/reduktor/passes/SimplifyBinaryExpression.kt
@@ -0,0 +1,25 @@
+package com.stepanov.bbf.reduktor.passes
+
+import com.stepanov.bbf.bugfinder.util.debugPrint
+import com.stepanov.bbf.bugfinder.util.getAllPSIChildrenOfType
+import com.stepanov.bbf.reduktor.executor.CompilerTestChecker
+import org.jetbrains.kotlin.psi.KtBinaryExpression
+import org.jetbrains.kotlin.psi.KtFile
+
+class SimplifyBinaryExpression(private val file: KtFile, private val checker: CompilerTestChecker) {
+    fun transform() {
+        file.getAllPSIChildrenOfType<KtBinaryExpression>().reversed().forEach { binExp ->
+            tryToReplace(binExp, true)
+        }
+        file.getAllPSIChildrenOfType<KtBinaryExpression>().reversed().forEach { binExp ->
+            tryToReplace(binExp, false)
+        }
+    }
+
+    //leftOrRight = true if left
+    private fun tryToReplace(binExp: KtBinaryExpression, leftOrRight: Boolean): Boolean {
+        val replacement = if (leftOrRight) binExp.left else binExp.right
+        replacement?.let { return checker.replaceNodeIfPossible(file, binExp, it) }
+        return false
+    }
+}
