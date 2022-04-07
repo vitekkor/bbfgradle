fbfb0c7938bf2b246513565d0a34831e2024414d
vitekkor <viktor.korotkikh@gmail.com>
2022-04-07 21:54:56 +0300

    complete inline and wrapped class transformation

--- src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/metamorphicTransformations/EquivalentTransformation.kt
+++ src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/metamorphicTransformations/EquivalentTransformation.kt
@@ -2,11 +2,11 @@ package com.stepanov.bbf.bugfinder.mutator.metamorphicTransformations
 
 import com.intellij.psi.PsiElement
 import com.stepanov.bbf.bugfinder.mutator.transformations.Factory
-import com.stepanov.bbf.bugfinder.util.addAfterThisWithWhitespace
-import com.stepanov.bbf.bugfinder.util.addAtTheEnd
-import com.stepanov.bbf.bugfinder.util.getRandomVariableName
-import com.stepanov.bbf.bugfinder.util.getTrue
+import com.stepanov.bbf.bugfinder.util.*
 import com.stepanov.bbf.reduktor.util.getAllPSIChildrenOfType
+import org.jetbrains.kotlin.psi.KtBinaryExpression
+import org.jetbrains.kotlin.psi.KtNameReferenceExpression
+import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.KtTypeReference
 import kotlin.random.Random
 
@@ -37,20 +37,61 @@ class EquivalentTransformation : MetamorphicTransformation() {
     private fun inlineClass() {
         val typeReferences = file.getAllPSIChildrenOfType<KtTypeReference>()
         val random = typeReferences.randomOrNull() ?: return
-        val inlineClassName = Random.getRandomVariableName(5)
-        val inlineClass = Factory.psiFactory.createClass("value class $inlineClassName(val inlineType: ${random.text})")
-
-        val inlineAnnotation = Factory.psiFactory.createAnnotationEntry("@JvmInline")
-        inlineAnnotation.addAfterThisWithWhitespace(inlineClass, "\n")
-
-        file.addAtTheEnd(inlineAnnotation)
-
-        /*val typeAliasReference = Factory.psiFactory.createType(inlineClassName)
-        typeReferences.forEach { it.replace(typeAliasReference) }*/
-
-        // TODO replace
+        val inlineClassName = "Inline_${Random.getRandomVariableName(5)}"
+        wrapOrInlineClass(inlineClassName, random, true)
     }
 
     private fun wrappedClass() {
+        val typeReferences = file.getAllPSIChildrenOfType<KtTypeReference>()
+        val random = typeReferences.randomOrNull() ?: return
+        val wrappedClassName = "Wrapped_${Random.getRandomVariableName(5)}"
+        wrapOrInlineClass(wrappedClassName, random, false)
+    }
+
+    private fun wrapOrInlineClass(className: String, wrappedInlinedType: KtTypeReference, isInlineClass: Boolean) {
+        val valueKeyWord = if (isInlineClass) "value" else ""
+        val propertyName = if (isInlineClass) "inlinedProperty" else "wrappedProperty"
+
+        val resultClass = Factory.psiFactory.createClass(
+            "$valueKeyWord class $className(val $propertyName: ${wrappedInlinedType.text}){ override fun toString(): String = $propertyName.toString() }"
+        )
+
+        if (isInlineClass) {
+            val inlineAnnotation = Factory.psiFactory.createAnnotationEntry("@JvmInline")
+            inlineAnnotation.addAfterThisWithWhitespace(resultClass, "\n")
+            file.addAtTheEnd(inlineAnnotation)
+        } else
+            file.addAtTheEnd(resultClass)
+
+        val prop =
+            file.getAllPSIChildrenOfType<KtProperty>().filter { it.getType(ctx!!)?.name == wrappedInlinedType.text }
+        prop.forEach { property ->
+            if (property.initializer != null) {
+                val newProperty =
+                    Factory.psiFactory.createProperty("${property.valOrVarKeyword.text} ${property.name}: $className = $className(${property.initializer!!.text})")
+                property.replaceThis(newProperty)
+            } else { // lateinit
+                val newProperty =
+                    Factory.psiFactory.createProperty("${property.valOrVarKeyword.text} ${property.name}: $className")
+                property.replaceThis(newProperty)
+            }
+        }
+        val ktNameReferenceExpression = file.getAllPSIChildrenOfType<KtNameReferenceExpression>()
+        prop.forEach { property ->
+            ktNameReferenceExpression.filter { it.text.contains(property.name!!) }.forEach { replacement ->
+                if (property.initializer != null)
+                    replacement.replaceThis(Factory.psiFactory.createExpression("${replacement.text}.$propertyName"))
+                else {
+                    val binaryExpression = (replacement.parent as? KtBinaryExpression)
+                    if (binaryExpression != null) {
+                        val newExpression =
+                            Factory.psiFactory.createExpression("${binaryExpression.left?.text} = $className(${binaryExpression.left?.text})")
+                        binaryExpression.replace(newExpression)
+                    } else {
+                        replacement.replaceThis(Factory.psiFactory.createExpression("${replacement.text}.$propertyName"))
+                    }
+                }
+            }
+        }
     }
 }
